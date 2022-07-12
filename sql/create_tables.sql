325d22a750fae98d2aab464e1874607ce2ca5bf2
prilutskiy <prilutskiy@ians.aero>
2022-07-12 03:39:58 +0300

    add: sql create tables, psql service in docker-compose

---
+++ sql/create_tables.sql
@@ -0,0 +1,44 @@
+CREATE TABLE library_user
+(
+  id VARCHAR(64) NOT NULL PRIMARY KEY,
+  username VARCHAR(20) NOT NULL,
+  email VARCHAR(20) NOT NULL,
+  password VARCHAR(256) NOT NULL
+);
+
+CREATE TABLE author
+(
+  id SERIAL PRIMARY KEY,
+  firstname VARCHAR(64) NOT NULL,
+  secondname VARCHAR(64) NOT NULL,
+  birthdate DATE NOT NULL
+);
+
+CREATE TABLE book
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(64) NOT NULL,
+  year DATE NOT NULL
+);
+
+CREATE TABLE book_copy
+(
+  id SERIAL PRIMARY KEY,
+  book_id INTEGER REFERENCES book(id)
+);
+
+CREATE TABLE book_author
+(
+  author_id INTEGER REFERENCES author(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  book_id INTEGER REFERENCES book(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT book_author_id PRIMARY KEY (author_id, book_id)
+);
+
+CREATE TABLE library_history
+(
+  id SERIAL PRIMARY KEY,
+  library_user_id VARCHAR(64) REFERENCES library_user(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  book_copy_id INTEGER REFERENCES book_copy(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  date_from DATE NOT NULL,
+  date_to DATE NOT NULL
+);
